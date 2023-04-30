f55c97f80c8d2b77aee726e3c47d90c8fdc5b1f4
Nicholas Zhang <nicholasz2510@gmail.com>
2023-04-30 13:09:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,17 +23,17 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def give(ctx, recipient: discord.Member, amount: int = 1):
-    _register(ctx.message.author.id)
+    register(ctx.message.author.id)
     if not points_data[str(ctx.message.author.id)]["is_rune"]:
         await ctx.send("Hey, you're not Rune! You can't do that!")
         return
-    _register(recipient.id)
+    register(recipient.id)
     if ctx.message.author.id == recipient.id:
         await ctx.send("Hmm... giving yourself some points?")
     points_data[str(recipient.id)]["rune_points"] += amount
     _save()
     await ctx.send("Wow! Rune just gave <@" + str(
-        recipient.id) + "> " + str(amount) + " Rune Points!")
+        recipient.id) + "> " + str(amount) + " Rune Point" + ("" if amount == 1 else "s") + "!")
 
 
 @give.error
@@ -45,17 +45,18 @@ async def give_error(ctx):
 
 @bot.command()
 async def remove(ctx, recipient: discord.Member, amount: int = 1):
-    _register(ctx.message.author.id)
+    register(ctx.message.author.id)
     if not points_data[str(ctx.message.author.id)]["is_rune"]:
         await ctx.send("Hey, you're not Rune! You can't do that!")
         return
-    _register(recipient.id)
+    register(recipient.id)
     if ctx.message.author.id == recipient.id:
         await ctx.send("Hmm... removing your own points?")
     points_data[str(recipient.id)]["rune_points"] -= amount
     _save()
-    await ctx.send(":( Rune just took away " + str(amount) + " Rune Points from <@" + str(
-        recipient.id) + ">!")
+    await ctx.send(
+        ":( Rune just took away " + str(amount) + " Rune Point" + ("" if amount == 1 else "s") + " from <@" + str(
+            recipient.id) + ">!")
 
 
 @remove.error
@@ -71,9 +72,11 @@ async def bal(ctx, recipient: discord.Member = None):
         discord_id = ctx.message.author.id
     else:
         discord_id = recipient.id
-    _register(discord_id)
+    register(discord_id)
     who = "They" if recipient else "You"
-    await ctx.send(who + " have " + str(points_data[str(discord_id)]["rune_points"]) + " Rune Points!")
+    await ctx.send(who + " have " + str(
+        points_data[str(discord_id)]["rune_points"]) + " Rune Point" + ("" if points_data[str(discord_id)][
+                                                                                 "rune_points"] == 1 else "s") + "!")
 
 
 @bot.command()
@@ -81,27 +84,45 @@ async def leaderboard(ctx):
     lb = []
     for user_id in points_data.keys():
         user = await bot.fetch_user(int(user_id))
-        lb.append((user.name + "#" + user.discriminator, points_data[user_id]["rune_points"]))
+        if points_data[user_id]["rune_points"] != 0:
+            lb.append((user.name + "#" + user.discriminator, points_data[user_id]["rune_points"]))
     lb.sort(key=lambda tup: tup[1], reverse=True)
     lb = lb[:10]
     lb_entries = []
     for i in range(len(lb)):
-        lb_entries.append(str(i + 1) + ". " + lb[i][0] + " (" + str(lb[i][1]) + " Rune Points)")
+        lb_entries.append(
+            str(i + 1) + ". " + lb[i][0] + " (" + str(lb[i][1]) + " Rune Point" + ("" if lb[i][1] == 1 else "s") + ")")
     await ctx.send("\n".join(lb_entries))
 
 
-def _save():
-    with open('points_data.json', 'w+') as file_save:
-        json.dump(points_data, file_save)
+@bot.command()
+async def loserboard(ctx):
+    lb = []
+    for user_id in points_data.keys():
+        user = await bot.fetch_user(int(user_id))
+        if points_data[user_id]["rune_points"] != 0:
+            lb.append((user.name + "#" + user.discriminator, points_data[user_id]["rune_points"]))
+    lb.sort(key=lambda tup: tup[1])
+    lb = lb[:10]
+    lb_entries = []
+    for i in range(len(lb)):
+        lb_entries.append(
+            str(i + 1) + ". " + lb[i][0] + " (" + str(lb[i][1]) + " Rune Point" + ("" if lb[i][1] == 1 else "s") + ")")
+    await ctx.send("\n".join(lb_entries))
 
 
-def _register(user_id):
+def register(user_id):
     discord_id = str(user_id)
     if discord_id not in points_data:
         points_data[discord_id] = {}
         points_data[discord_id]["rune_points"] = 0
         points_data[discord_id]["is_rune"] = False
         _save()
+
+
+def _save():
+    with open('points_data.json', 'w+') as file_save:
+        json.dump(points_data, file_save)
 
 
 with open('api_key.txt', 'r') as f:
